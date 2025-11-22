e29c784f3322d92601573ff233423f9a86da0b96
Srflowthers <maulestyle@gmail.com>
2025-11-22 18:25:18 -0300

    Definicion Layout

--- principal/models.py
+++ principal/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.core.validators import RegexValidator
 from django.urls import reverse
 from django.contrib.auth.models import User
-
 import uuid
 
 # Validador para RUT
